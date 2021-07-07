780e546e9d56afde63560a9485fe7f1f4d083761
JiunJung <66217921+JiunJung@users.noreply.github.com>
2021-07-08 00:26:06 +0900

    my new idea

--- js/background.js
+++ js/background.js
@@ -1,7 +1,3 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-const randomImage = images[Math.floor(Math.random() * images.length)];
-
-const bgrImage = document.createElement('img'); //요소 노드가 생성되어있는 상태이긴 하지만 아직 문서의 부모노드 밑에 연결되지는 않음.
-bgrImage.src = `img/${randomImage}`; //이미지 랜덤생성
-
-document.getElementById("container").prepend(bgrImage);
+const bodyColorArray = ["body-color-pink", "body-color-skyblue", "body-color-salmon"];
+const bodyColor = bodyColorArray[Math.floor(Math.random() * bodyColorArray.length)];
+document.body.classList.add(bodyColor);
